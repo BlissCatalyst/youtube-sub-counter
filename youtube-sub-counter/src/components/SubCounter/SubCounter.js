852a71b604808b3b5b0aeb93199bb12ebe707f63
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-12-23 19:16:24 -0700

    axios call to youtube api getting channel stats

--- youtube-sub-counter/src/components/SubCounter/SubCounter.js
+++ youtube-sub-counter/src/components/SubCounter/SubCounter.js
@@ -5,11 +5,28 @@ export default class SubCounter extends Component {
     constructor() {
         super();
         this.state = {
+            channel_stats: {},
             error: ""
         };
     }
 
-    componentDidMount() {}
+    componentDidMount() {
+        axios({
+            method: "get",
+            url: "https://www.googleapis.com/youtube/v3/channels",
+            params: {
+                key: "AIzaSyAcX2XGaPYMrZe5PbWaSlA-KXiVGqrkEpg",
+                part: statistics,
+                id: "UCJwnN8Kmor1p97kEY_j4OLg"
+            }
+        })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
     render() {
         return <div></div>;
