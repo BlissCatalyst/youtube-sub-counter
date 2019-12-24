dec0977922d15e0e4f588e343f31148e40dddc0d
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-12-23 18:57:09 -0700

    created SubCounter class

---
+++ youtube-sub-counter/src/components/SubCounter/SubCounter.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class SubCounter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            error: ""
+        };
+    }
+
+    componentDidMount() {}
+
+    render() {
+        return <div></div>;
+    }
+}
