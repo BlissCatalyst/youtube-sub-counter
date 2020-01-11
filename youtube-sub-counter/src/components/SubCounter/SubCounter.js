2b092881bb584dd6fb797e2b7adbbbf2bcb045cf
Logan Hufstetler <logan.hufstetler@gmail.com>
2020-01-10 23:14:20 -0700

    TODO- attach to backend API

--- youtube-sub-counter/src/components/SubCounter/SubCounter.js
+++ youtube-sub-counter/src/components/SubCounter/SubCounter.js
@@ -16,7 +16,7 @@ export default class SubCounter extends Component {
             method: "get",
             url: "https://www.googleapis.com/youtube/v3/channels",
             params: {
-                key: "AIzaSyAcX2XGaPYMrZe5PbWaSlA-KXiVGqrkEpg",
+                key: "AIzaSyAcX2XGaPYMrZe5PbWaSlA-KXiVGqrkEpg", // TODO: Attach this to backend API
                 part: "statistics",
                 // Should make id load dynamically based on channel search
                 id: "UCJwnN8Kmor1p97kEY_j4OLg"
